f986386f276e773b2f758a7354bcd8d94e77e1e6
misarb <lahssane12@gmail.com>
2023-01-16 12:17:15 +0100

    Algo Update erreur optimizer

--- neural_network.m
+++ neural_network.m
@@ -1,6 +1,5 @@
-  function [z4]=  neural_network(consg, err)
-   
-    epochs = 5;
+function [z4,z3,z2,cost] =  neural_network(consg, err)
+    epochs = 50;
     learningRate = 0.1;
     w1 = 1;
     b1 = 0.1;
@@ -10,22 +9,29 @@
     b3 = 0.1;
     w4 = 1;
     b4 = 0.1;
-    for i=0:epochs
-        [a1,z1 ,a2,z2,a3,z3,a4,z4] = forward(err,w1,w2,w3,w4,b1,b2,b3,b4);
+    for i=1:epochs
+        [a1,z1 ,a2,z2,a3,z3,a4,z4] = forward(err,consg,w1,w2,w3,w4,b1,b2,b3,b4);
         %w2,a1,z0,z1,z2,y
         [theata1,theata2,theata3,theata4 ]= backProbagation(w2,w3,w4,a4,z3,z2,z1,consg);
-        % Update weights and biases
-        w4 = w4 - learningRate * theata4 .* a3';
-        b4 = b4 - learningRate * theata4;
-        w3 = w3 - learningRate * theata3 * a2';
-        b3 = b3 - learningRate * theata3;
-        w2 = w2 - learningRate * theata2 .* a1';
-        b2 = b2 - learningRate * theata2;
-        w1 = w1 - learningRate * theata1 * err;
-        b1 = b1 - learningRate * theata1;                           
-    end                            
-    
-end                          
-                           
-%backProbagation(w2,a1,z1,a2,z2,a3,z3,a4,z4,consg)                             
-%backProbagation(w2,a1,z0,z1,z2,y)                             
+%         w1 = w1 - learningRate*Adam(theata1,w1);
+%         w2 = w2 - learningRate*Adam(theata2,w2);
+%         w3 = w3 - learningRate*Adam(theata3,w3);
+%         w4 = w4 - learningRate*Adam(theata4,w4);        
+%         b1 = b1 - learningRate*Adam(theata1,b1);
+%         b2 = b2 - learningRate*Adam(theata2,b2);
+%         b3 = b3 - learningRate*Adam(theata3,b3);
+%         b4 = b4 - learningRate*Adam(theata4,b4);
+        w4 = w4 - learningRate *theata4.*a3';
+        b4 = b4 - learningRate *theata4;
+        w3 = w3 - learningRate *theata3.*a2';
+        b3 = b3 - learningRate *theata3;
+        w2 = w2 - learningRate *theata2.*a1';
+        b2 = b2 - learningRate *theata2;
+        w1 = w1 - learningRate *theata1.*err;
+        b1 = b1 - learningRate *theata1;  
+        % Adaptive learning rate
+        %learningRate = learningRate / (1 + i * 0.01);
+        cost = (1 / 2) * (z4 - consg)^2;
+    end                              
+end 
+
--- sigmoidGradient.m
+++ sigmoidGradient.m
@@ -1,10 +1,5 @@
 function g = sigmoidGradient(z)
 %% Segmoide Function
 g = sigmoid(z).*(1-sigmoid(z));
-
 % =============================================================
-
-
-
-
 end
